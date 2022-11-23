de7687e49c8c7b974c26dc26ee6a21fc483536c6
Isabela <72067839+Mocanu-Isabela@users.noreply.github.com>
2022-11-23 12:10:54 +0200

    doc lab4

--- Lab4/main.py
+++ Lab4/main.py
@@ -4,9 +4,6 @@ class Transition:
         self.destination = destination
         self.symbol = symbol
 
-    def to_string(self):
-        return self.source + "->" + self.destination + " :" + self.symbol
-
 
 class Run:
     states = []
@@ -15,17 +12,19 @@ class Run:
     final_states = []
     transitions = []
 
-    def get_all_from_file(self, transitions_from_FA, f_states_from_FA, i_state_from_FA):
+    def get_all_from_file(self, states_from_FA, alphabet_from_FA, transitions_from_FA, f_states_from_FA, i_state_from_FA):
+        self.states = states_from_FA
+        self.alphabet = alphabet_from_FA
+
         for t in transitions_from_FA:
             trans = Transition(str(t[0]), str(t[1]), str(t[2]))
             if not self.transition_already_exists(trans):
                 self.transitions.append(trans)
-                self.alphabet.append(trans)
 
         for f_s in f_states_from_FA:
             self.final_states.append(f_s)
 
-        self.initial_state = i_state_from_FA
+        self.initial_state = i_state_from_FA[0]
 
     def transition_already_exists(self, trans):
         for transition in self.transitions:
@@ -41,20 +40,12 @@ class Run:
                 break
         return destination
 
-    def is_DFA(self):
-        for state in self.states:
-            values = []
-            for transition in self.transitions:
-                if transition.source == state:
-                    values.append(transition.symbol)
-            for v1 in range(len(values) - 1):
-                for v2 in range(v1, len(values)):
-                    if values[v1] == values[v2]:
-                        return False
-            return True
-
     def verify_sequence(self, seq):
-        current_state = self.initial_state[0]
+        if len(seq) == 0:
+            if self.initial_state in self.final_states:
+                return True
+            return False
+        current_state = self.initial_state
         for s in seq:
             state = self.get_destination(current_state, s)
             if state == "":
@@ -82,6 +73,7 @@ def print_menu():
 def start(states, alphabet, initial_state, final_states, transitions):
     stop = False
     run = Run()
+    run.get_all_from_file(states, alphabet, transitions, final_states, initial_state)
     while not stop:
         print_menu()
         ui_command = input("Please enter a command: ")
@@ -98,7 +90,6 @@ def start(states, alphabet, initial_state, final_states, transitions):
                 printer += str(state)
                 printer += " "
             print(printer)
-            pass
         elif c[0] == '2':
             printer = "Alphabet: "
             for alpha in alphabet:
@@ -106,18 +97,15 @@ def start(states, alphabet, initial_state, final_states, transitions):
 
                 printer += " "
             print(printer)
-            pass
         elif c[0] == '3':
             printer = "Initial state: " + str(initial_state[0])
             print(printer)
-            pass
         elif c[0] == '4':
             printer = "Final states: "
             for f_state in final_states:
                 printer += str(f_state)
                 printer += " "
             print(printer)
-            pass
         elif c[0] == '5':
             printer = "Transitions: \n"
             for tran in transitions:
@@ -125,21 +113,14 @@ def start(states, alphabet, initial_state, final_states, transitions):
                 tran_to_str = str(tran[0]) + "->" + str(tran[1]) + " :" + str(tran[2]) + "\n"
                 printer += str(tran_to_str)
             print(printer)
-            pass
         elif c[0] == '6':  # 110 ok
             print("Please enter a sequence: ")
             sequence = input()
-            run.get_all_from_file(transitions, final_states, initial_state)
-            # is_deterministic = run.is_DFA()
-            # if is_deterministic:
             verif = run.verify_sequence(sequence)
             if verif:
                 print("The sequence is accepted by the FA      YEEEEEY :)")
             else:
                 print("The sequence is NOT accepted by the FA")
-            # else:
-            #     print("Not DFA!")
-            # pass
 
 
 if __name__ == '__main__':
